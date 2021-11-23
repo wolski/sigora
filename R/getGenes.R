ce0553f8778b33a7df449b948505d6b375cadaa6
Witold Wolski <wewolski@gmail.com>
2021-11-23 12:49:13 +0100

    doc

--- R/getGenes.R
+++ R/getGenes.R
@@ -34,9 +34,9 @@ getGenes <- function(yy, i, idmap = load_data('idmap')) {
                           FALSE]
   ## pgs<-unique(as.character(as.vector(yy1[yy1[,4]==1,1:2])))
   ## print(pgs)
-  r1 <- aggregate(as.numeric(yy1[, 4]), by = list(yy1[, 1]), FUN = sum)
-  r2 <- aggregate(as.numeric(yy1[, 4]), by = list(yy1[, 2]), FUN = sum)
-  r3 <- aggregate(rbind(r1, r2)[, 2], by = list(rbind(r1, r2)[, 1]), FUN =
+  r1 <- stats::aggregate(as.numeric(yy1[, 4]), by = list(yy1[, 1]), FUN = sum)
+  r2 <- stats::aggregate(as.numeric(yy1[, 4]), by = list(yy1[, 2]), FUN = sum)
+  r3 <- stats::aggregate(rbind(r1, r2)[, 2], by = list(rbind(r1, r2)[, 1]), FUN =
                     sum)
   r4 <- as.data.frame(r3[order(-r3[, 2]), ])
   r4[, 2] <- 0.5 * r4[, 2]
--- R/load_data.R
+++ R/load_data.R
@@ -1,4 +1,8 @@
-load_data <- function(datastr, package="sigora"){
-  data(list = datastr, package = package)
+#' load data when lazyLoad true.
+#' @param datastr name of datasets
+#' @param package default sigora
+#' @export
+load_data <- function(datastr, package = "sigora") {
+  utils::data(list = datastr, package = package)
   return(eval(parse(text = datastr)))
 }
