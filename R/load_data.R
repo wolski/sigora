787e9ade9a4bd6770ad0ef622f9abda45b3a5f34
Witold Wolski <wewolski@gmail.com>
2021-11-24 13:02:50 +0100

    document return value

--- R/load_data.R
+++ R/load_data.R
@@ -1,6 +1,7 @@
-#' load data when lazyLoad true.
+#' load and return data when lazyLoad false insted of using data(datastr)
 #' @param datastr name of datasets
 #' @param package default sigora
+#' @return returns the data
 #' @export
 load_data <- function(datastr, package = "sigora") {
   utils::data(list = datastr, package = package)
